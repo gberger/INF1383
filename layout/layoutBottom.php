201faa7d42645b1bf68a0ea5125b9e6e424bf1a9
Guilherme Berger <guilherme.berger@gmail.com>
2013-11-20 19:19:01 -0200

    Inclui tradução para Português do dataTables no layoutBottom

--- layout/layoutBottom.php
+++ layout/layoutBottom.php
@@ -6,5 +6,6 @@
     <script src="/assets/js/bootstrap.min.js"></script>
     <script src="/assets/js/jquery.dataTables.min.js"></script>
     <script src="/assets/js/dataTables.bootstrap.js"></script>
+    <script src="/assets/js/dataTables.defaults.js"></script>
   </body>
 </html>

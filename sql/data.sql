d1dacb3e9e0677d310b3c8c3fa602b9a67447e26
Gabriel Siqueira <gabrielsiq@msn.com>
2013-12-05 23:22:14 -0200

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,24 +1,24 @@
 INSERT INTO emissor_rg VALUES
-	('SSP', 'Secretaria de Segurança Pública'),
-	('PM', 'Polícia Militar'),
-	('PC', 'Policia Civil'),
-	('CNT', 'Carteira Nacional de Habilitação'),
-	('DIC', 'Diretoria de Identificação Civil'),
-	('CTPS', 'Carteira de Trabaho e Previdência Social'),
-	('FGTS', 'Fundo de Garantia do Tempo de Serviço'),
-	('IFP', 'Instituto Félix Pacheco'),
-	('IPF', 'Instituto Pereira Faustino'),
-	('IML', 'Instituto Médico-Legal'),
-	('MTE', 'Ministério do Trabalho e Emprego'),
-	('MMA', 'Ministério da Marinha'),
-	('MAE', 'Ministério da Aeronáutica'),
-	('MEX', 'Ministério do Exército'),
-	('POF', 'Polícia Federal'),
-	('POM', 'Polícia Militar'),
-	('SES', 'Carteira de Estrangeiro'),
-	('SJS', 'Secretaria da Justiça e Segurança'),
-	('SJTS', 'Secretaria da Justiça do Trabalho e Segurança'),
-	('ZZZ', 'Outros (inclusive exterior)')
+        ('SSP', 'Secretaria de Segurança Pública'),
+        ('PM', 'Polícia Militar'),
+        ('PC', 'Policia Civil'),
+        ('CNT', 'Carteira Nacional de Habilitação'),
+        ('DIC', 'Diretoria de Identificação Civil'),
+        ('CTPS', 'Carteira de Trabaho e Previdência Social'),
+        ('FGTS', 'Fundo de Garantia do Tempo de Serviço'),
+        ('IFP', 'Instituto Félix Pacheco'),
+        ('IPF', 'Instituto Pereira Faustino'),
+        ('IML', 'Instituto Médico-Legal'),
+        ('MTE', 'Ministério do Trabalho e Emprego'),
+        ('MMA', 'Ministério da Marinha'),
+        ('MAE', 'Ministério da Aeronáutica'),
+        ('MEX', 'Ministério do Exército'),
+        ('POF', 'Polícia Federal'),
+        ('POM', 'Polícia Militar'),
+        ('SES', 'Carteira de Estrangeiro'),
+        ('SJS', 'Secretaria da Justiça e Segurança'),
+        ('SJTS', 'Secretaria da Justiça do Trabalho e Segurança'),
+        ('ZZZ', 'Outros (inclusive exterior)')
 ;
 
 INSERT INTO filial VALUES
@@ -39,7 +39,9 @@ INSERT INTO pessoa VALUES
 ('88884721261', 'Vera Stein', '1944-02-22', NULL, NULL, 'R. JBK, 12 - Liberdade - São Paulo - SP', '663322', 'IML', 'São Paulo', 'casado', 'F', 'Enfermeiro', 'vs@gmail.com', 'B-', NULL, '87979879', NULL, 'superior completo', NULL),
 ('41568633360', 'Ana Lucia', '1959-02-12', NULL, NULL, 'R. CAB, 22 - Salvador - BA', '142536', 'SSP', 'São Paulo', 'divorciado', 'F', 'Professor', 'al@outlook.com', 'AB-', 'B', '85687495', NULL, 'superior completo', NULL),
 ('46476479431', 'Pedro Mendes', '1945-01-15', NULL, NULL, 'R. ABC, 11 - Fortaleza - CE', '461379', 'POM', 'Fortaleza', 'casado', 'M', 'Dentista', 'pm@gmail.com', 'O-', 'A', '89988658', NULL, 'superior completo', NULL),
-('19534157848', 'Maria Augusta', '1966-12-02', NULL, NULL, 'R. CBD, 14 - Brasília - DF', '712634', 'POF', 'Maceió', 'casado', 'F', 'Cozinheiro', 'ma@hotmail.com', 'O-', NULL, '75489674', NULL, 'fundamental incompleto', NULL)
+('19534157848', 'Maria Augusta', '1966-12-02', NULL, NULL, 'R. CBD, 14 - Brasília - DF', '712634', 'POF', 'Maceió', 'casado', 'F', 'Cozinheiro', 'ma@hotmail.com', 'O-', NULL, '75489674', NULL, 'fundamental incompleto', NULL),
+('78992137540', 'Roberto Menezes', '1967-12-02', NULL, NULL, 'R. CBD, 15 - Brasília - DF', '164825', 'POF', 'Recife', 'solteiro', 'M', 'Advogado', 'rm@gmail.com', 'O+', NULL, '46521759', NULL, 'fundamental completo', NULL)
+
 ;
 
 INSERT INTO funcionario VALUES
@@ -48,8 +50,8 @@ INSERT INTO funcionario VALUES
 ;
 
 INSERT INTO atividade VALUES
-(DEFAULT, 1, '12345678909', 'Evento', '2013-01-01', 'Praça São Fulano da Silva - Centro - Rio de Janeiro', 'Atividade da CVB'),
-(DEFAULT, 2, '17283624902', 'Arrecadação de Alimentos', '2013-07-05', 'Praça do Feijão - Liberdade - São Paulo', 'Arrecadação de alimentos para os desabrigados das chuvas'),
+(DEFAULT, 1, '12345678909', 'Evento', '2013-08-21', 'Praça São Fulano da Silva - Centro - Rio de Janeiro', 'Atividade da CVB'),
+(DEFAULT, 2, '17283624902', 'Doação de Alimentos', '2013-07-23', 'Praça do Feijão - Liberdade - São Paulo', 'Arrecadação de alimentos para os desabrigados das chuvas'),
 (DEFAULT, 1, '28246103440', 'JMJ', '2013-07-13', 'Base Aerea de Santa Cruz - Santa Cruz - Rio de Janeiro', 'Jornada mundial da juventude'),
 (DEFAULT, 1, NULL, 'Evento 01', '2013-01-13', 'Rio de Janeiro', 'Evento de demonstraçao'),
 (DEFAULT, 1, NULL, 'Evento 02', '2013-02-20', 'Rio de Janeiro', 'Evento de demonstraçao'),
@@ -142,7 +144,8 @@ INSERT INTO lingua VALUES
 (DEFAULT, 'Espanhol','Argentina'),
 (DEFAULT, 'Inglês','EUA'),
 (DEFAULT, 'Inglês','UK'),
-(DEFAULT, 'Inglês','África do Sul')
+(DEFAULT, 'Inglês','África do Sul'),
+(DEFAULT, 'Mandarim', 'China')
 ;
 
 INSERT INTO fala VALUES
@@ -151,7 +154,8 @@ INSERT INTO fala VALUES
 ('28246103440',1,'iniciante'),
 ('74114767223',4,'fluente'),
 ('17283624902',8,'medio'),
-('41568633360',5,'iniciante')
+('41568633360',5,'iniciante'),
+('46476479431',7, 'medio'),
+('46476479431',9,'iniciante'),
+('78992137540',6, 'fluente')
 ;
-
-
